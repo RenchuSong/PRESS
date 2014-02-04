c85436f7a0272cdc50fd56d7e81356a20645fe03
RenchuSong <songrenchu@sina.com>
2014-02-04 15:49:18 +0800

    Initial Commit

---
+++ v2/PRESS-v2/PRESS-v2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PRESS-v2
+//
+//  Created by Renchu Song on 4/2/14.
+//  Copyright (c) 2014 Renchu Song. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
