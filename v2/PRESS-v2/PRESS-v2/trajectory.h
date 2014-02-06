721018c72950d4e54c0b91a2fd49fe96b43c44aa
RenchuSong <songrenchu@sina.com>
2014-02-06 17:50:36 +0800

    Road network trajectory

--- v2/PRESS-v2/PRESS-v2/trajectory.h
+++ v2/PRESS-v2/PRESS-v2/trajectory.h
@@ -36,18 +36,69 @@ public:
 		this->pointNumber = fr->nextInt();
 		this->sequence.clear();
 		for (int i = 0; i < this->pointNumber; ++i) {
-			GPSPoint p(fr->nextInt(), fr->nextDouble(), fr->nextDouble());
-			this->sequence.push_back(p);
+			GPSPoint point(fr->nextInt(), fr->nextDouble(), fr->nextDouble());
+			this->sequence.push_back(point);
 		}
 	}
 	
-	/* The way to print the GPS trajectory
-	void printTrajectory() {
-		cout << this->pointNumber << endl;
+	//Store a GPS trajectory
+	void storeTrajectory(FileWriter* fw) {
+		fw->writeInt(this->pointNumber);
 		for (int i = 0; i < this->pointNumber; ++i) {
-			cout << sequence[i].t << " " << sequence[i].x << " " << sequence[i].y << endl;
+			fw->writeInt(sequence[i].t);
+			fw->writeDouble(sequence[i].x);
+			fw->writeDouble(sequence[i].y);
 		}
-	}*/
+	}
+	
+};
+
+// Road network trajectory in PRESS
+//Temporal representation in (d, t) pair
+struct TemporalPair {
+	double d;
+	int t;
+	TemporalPair(int t, double d) {
+		this->t = t;
+		this->d = d;
+	}
+};
+
+class RoadNetTrajectory {
+public:
+	int spatialNumber, temporalNumber;
+	vector<int> spatial;
+	vector<TemporalPair> temporal;
+	
+	//Construct a road network trajectory from Spatial File Reader and Temporal File Reader
+	RoadNetTrajectory(FileReader* spatialReader, FileReader* temporalReader) {
+		//Construct spatial path
+		this->spatialNumber = spatialReader->nextInt();
+		this->spatial.clear();
+		for (int i = 0; i < this->spatialNumber; ++i) {
+			this->spatial.push_back(spatialReader->nextInt());
+		}
+		//Construct temporal sequence
+		this->temporalNumber = temporalReader->nextInt();
+		this->temporal.clear();
+		for (int i = 0; i < this->temporalNumber; ++i) {
+			TemporalPair dtPair(temporalReader->nextInt(), temporalReader->nextDouble());
+			this->temporal.push_back(dtPair);
+		}
+	}
+	
+	//Store a road network trajectory
+	void storeTrajectory(FileWriter* spatialWriter, FileWriter* temporalWriter) {
+		spatialWriter->writeInt(this->spatialNumber);
+		for (int i = 0; i < this->spatialNumber; ++i) {
+			spatialWriter->writeInt(spatial[i]);
+		}
+		temporalWriter->writeInt(this->temporalNumber);
+		for (int i = 0; i < this->temporalNumber; ++i) {
+			temporalWriter->writeInt(temporal[i].t);
+			temporalWriter->writeDouble(temporal[i].d);
+		}
+	}
 };
 
 #endif
