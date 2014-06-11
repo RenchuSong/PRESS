b62dd83c5a95297601a156782ad7fd3689a859f4
RenchuSong <songrenchu@sina.com>
2014-06-10 20:44:24 -0700

    WHENat calculate distance

--- v2/PRESS-v2/PRESS-v2/query.h
+++ v2/PRESS-v2/PRESS-v2/query.h
@@ -58,7 +58,10 @@ public:
 			double mid = 0;
 			Edge* edge = graph->getEdge(trajectory->spatial->at(i));
 			if (auxiliary->spMBR[edge->startNode->id][edge->endNode->id]->contain(queryLocation)) {
-				throw "Not Implemented Yet!";//TODO
+				mid = bias(edge->geometry, queryLocation);
+				if (mid != Config::NULL_POINTER) {
+					flag = true;
+				}
 			}
 			if (!flag) {
 				d += edge->len;
