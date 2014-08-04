992b9462037b416f68f45ceb5ca9f18df46ac55e
RenchuSong <songrenchu@sina.com>
2014-08-04 23:05:33 +0800

    create test files

---
+++ v2/PRESS-v2/PRESS-v2/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  PRESS-v2
+//
+//  Created by Renchu Song on 4/8/14.
+//  Copyright (c) 2014 Renchu Song. All rights reserved.
+//
+
+#ifndef PRESS_v2_test_h
+#define PRESS_v2_test_h
+
+
+
+#endif
---
+++ v2/PRESS-v2/PRESS-v2/test_base.h
@@ -0,0 +1,14 @@
+//
+//  test_base.h
+//  PRESS-v2
+//
+//  Created by Renchu Song on 4/8/14.
+//  Copyright (c) 2014 Renchu Song. All rights reserved.
+//
+
+#ifndef PRESS_v2_test_base_h
+#define PRESS_v2_test_base_h
+
+
+
+#endif
