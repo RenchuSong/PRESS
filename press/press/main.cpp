cd5bcb8246211a5ead90d0c86b955d5936c33c2e
RenchuSong <songrenchu@sina.com>
2016-01-17 23:15:38 -0800

    - Restructure folder, create new folder

---
+++ press/press/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  press
+//
+//  Created by Renchu Song on 17/1/16.
+//  Copyright © 2016 Renchu Song. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
