b22f9deec844439522d3ac968adc8a67a39c81d2
RenchuSong <songrenchu@sina.com>
2018-05-13 16:59:24 -0700

    Initial code commit.

---
+++ core/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  press-v3
+//
+//  Created by Renchu Song on 5/13/18.
+//  Copyright © 2018 Renc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
