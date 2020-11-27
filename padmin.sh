64dc96023b31b91467a6dad676b4814df3b0d2d4
RenchuSong <songrenchu@fudan.edu.cn>
2020-11-26 20:33:33 -0800

    Add script to run test client.

--- padmin.sh
+++ padmin.sh
@@ -50,26 +50,3 @@ case ${option} in
         exit 1
         ;;
 esac
-
-# printf "${BLUE}Building PRESS Core ...${NC}\n"
-# cd ${DIR_CORE}/src
-# g++ -o ${DIR}/bin/press_core_d -std=c++11 main.cpp */*.cpp */*/*.cpp
-# fail_on_err "Building PRESS Core Failed"
-# cd ${DIR}
-
-# printf "${BLUE}Building PRESS Test Client ...${NC}\n"
-# cd ${DIR_CORE}/src
-# g++ -o ${DIR}/bin/press_core_test_client -std=c++11 press_core_test_client.cpp */*.cpp */*/*.cpp
-# fail_on_err "Building PRESS Test Client Failed"
-# cd ${DIR}
-
-# printf "${BLUE}Generating Config File ...${NC}\n"
-# sed "s+\[ROOT\]+${DIR}+g" ${DIR}/config_template.json > ${DIR}/config.json
-# fail_on_err "Failed to generate config file"
-
-# printf "${BLUE}Downloading Sample Data ...${NC}\n"
-# cd ${DIR}/data
-# bash download_data.sh
-# cd ${DIR}
-
-# printf "${GREEN}Installation Finished${NC}\n"
---
+++ run_press_test_client.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+RED='\033[1;31m'
+BLUE='\033[1;36m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+DIR=${PWD}
+DIR_CORE=${DIR}/core
+
+function fail_on_err {
+    if [ $? -ne 0 ]; then
+        printf "${RED}$1${NC}\n"
+        exit
+    fi
+}
+
+function check_press_core {
+    pid="$(ps aux | grep press_core_d | grep -v 'grep' | cut -d' ' -f2- | xargs | cut -d' ' -f1)"
+    if [[ ${pid} -eq '' ]]
+    then
+        printf "${RED}Please run './padmin.sh start' first!${NC}\n"
+        exit
+    fi
+}
+
+check_press_core
+printf "${BLUE}Start running PRESS test client ...${NC}\n"
+${DIR}/bin/press_core_test_client ${DIR}/PRESS_CORE_OUT ${DIR}/PRESS_CORE_IN
+fail_on_err "Failed to run PRESS test client."
+printf "${BLUE}Finished running PRESS test client ...${NC}\n"
