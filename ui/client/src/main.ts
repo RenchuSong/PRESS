b53699dea9ea5e8157df544911d8006f8c8d4812
RenchuSong <songrenchu@fudan.edu.cn>
2020-12-27 13:53:39 -0800

    Install ant-design-vue@next

--- ui/client/src/main.ts
+++ ui/client/src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { key, store } from "./store";
+import DatePicker from 'ant-design-vue/lib/date-picker';
+import "ant-design-vue/dist/antd.css"
 
 createApp(App)
   .use(store, key)
   .use(router)
+  .use(DatePicker)
   .mount("#app");
