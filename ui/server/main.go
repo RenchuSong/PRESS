d8b57e9cf4595d1c6fb2a2c6492220a2c8521b52
RenchuSong <songrenchu@fudan.edu.cn>
2021-01-17 18:19:36 -0800

    Improve config template, and add config marshal logic

--- ui/server/main.go
+++ ui/server/main.go
@@ -1,12 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 )
 
+// PRESS config.
+type config struct {
+	Experiments string          `json:"experiments"`
+	Data        string          `json:"data"`
+	AppServer   appServerConfig `json:"appServer"`
+}
+
+// App server config
+type appServerConfig struct {
+	Logs           string `json:"logs"`
+	LogLevel       string `json:"logLevel"`
+	Port           int    `json:"port"`
+	StaticHandlers int    `json:"staticHandlers"`
+	APIHandlers    int    `json:"apiHandlers"`
+}
+
 func main() {
+	var confPath string
+	flag.StringVar(&confPath, "config", "./config.json", "the config file path")
+	flag.Parse()
+	data, err := ioutil.ReadFile(confPath)
+	if err != nil {
+		log.Fatalf(
+			"failed to load config file %v: %v",
+			confPath,
+			err,
+		)
+	}
+	var c config
+	if err := json.Unmarshal(data, &c); err != nil {
+		log.Fatalf(
+			"failed to unmarshal the config file: %v",
+			err,
+		)
+	}
+	str, err := json.Marshal(c)
+
+	log.Infof(string(str))
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
